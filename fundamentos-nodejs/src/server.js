8e5746a66d384dc7adb1994b38dc6629abdb7f79
Murillou <Murillovinicius13@hotmail.com>
2024-05-26 23:24:29 -0300

    [feat] fundamentos do Node

---
+++ fundamentos-nodejs/src/server.js
@@ -0,0 +1,27 @@
+import http from 'node:http';
+
+const users = [];
+
+const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url == '/users') {
+    users.push({
+      id: 1,
+      name: 'Murillo',
+      email: 'Murillovinicius13@hotmail.com',
+    });
+
+    return response.end('Usuário criado');
+  }
+
+  return response.end('Hello Ignite!');
+});
+
+server.listen(3333);
