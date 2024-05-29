8f4dea57d85d97e3fcb825617165db0f27ab435d
Murillou <murillovinicius13@hotmail.com>
2024-05-28 23:28:42 -0300

    [feat] adicionado put e delete

--- fundamentos-nodejs/src/database.js
+++ fundamentos-nodejs/src/database.js
@@ -1,3 +1,4 @@
+import { throws } from 'node:assert';
 import fs from 'node:fs/promises';
 
 const databasePath = new URL('../db.json', import.meta.url);
@@ -34,5 +35,22 @@ export class Database {
     this.#persist();
 
     return data;
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data };
+      this.#persist();
+    }
+  }
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
   }
 }
--- fundamentos-nodejs/src/routes.js
+++ fundamentos-nodejs/src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from './database.js';
 import { randomUUID } from 'node:crypto';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database();
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const users = database.select('users');
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const { name, email } = request.body;
 
@@ -31,10 +32,23 @@ export const routes = [
     },
   },
   {
-    method: 'DELETE',
-    path: `/users/${randomUUID}`,
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
     handler: (request, response) => {
-      return response.end();
+      const id = request.params.id;
+      const { name, email } = request.body;
+
+      database.update('users', id, { name, email });
+      return response.writeHead(204).end();
+    },
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (request, response) => {
+      const id = request.params.id;
+      database.delete('users', id);
+      return response.writeHead(204).end();
     },
   },
 ];
